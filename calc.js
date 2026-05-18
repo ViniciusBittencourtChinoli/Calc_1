0c45cc9e500744b278f3ae7967eedc09b28a4a7e
Vinicius Bittencourt Chinoli <itzvina1402@gmail.com>
2026-05-18 18:21:08 -0300

    codigo finalizado

--- calc.js
+++ calc.js
@@ -1,3 +1,12 @@
+var op
+var num1
+var num2
+var readlineSync = require('readline-sync');
+
+op = readlineSync.question("Operacao matematica (+) (-) (*) (/)? : \n");
+num1 = parseFloat(readlineSync.question("Insira o primeiro numero: \n"));
+num2 = parseFloat(readlineSync.question("Insira o segundo numero: \n"));
+
 function calculadora(num1, num2, op) {
     if (op === '+' ) {
         return num1 + num2;
@@ -15,3 +24,5 @@ function calculadora(num1, num2, op) {
         return 'Operacao invalida';
     }
 }
+
+console.log('O resultado é', calculadora(num1, num2, op));
